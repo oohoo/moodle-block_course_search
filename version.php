16aae70efff5b4ab0f97cc7ccbb4d75177f297c5
Nicolas Bretin <bretin@ualberta.ca>
2012-12-10 15:17:13 -0700

    Update the logo for Moodle 2.4

--- version.php
+++ version.php
@@ -16,9 +16,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2012103100;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2012121000;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2011112900;        // Requires this Moodle version
 $plugin->component = 'block_course_search';      // Full name of the plugin (used for diagnostics)
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.0.0 (Build: 2012103100)';
+$plugin->release = '2.0.0 (Build: 2012121000)';
 $plugin->cron = 0;
